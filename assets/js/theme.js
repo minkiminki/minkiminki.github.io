075e80fab6203fbf55e3aa2d98727b3d2fc55d9b
Minki Cho <minki.cho@sf.snu.ac.kr>
2023-07-03 07:43:47 +0900

    Set dark mode as default

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -75,7 +75,8 @@ let transTheme = () => {
 let initTheme = (theme) => {
   if (theme == null || theme == "null") {
     const userPref = window.matchMedia;
-    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+    if (true) { // dark mode is default
+    // if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
       theme = "dark";
     }
   }
